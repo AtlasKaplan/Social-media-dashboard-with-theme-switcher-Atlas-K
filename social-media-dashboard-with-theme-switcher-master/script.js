fd9bac922bca4323211fe88acb4727a374c1a313
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-05-13 16:32:46 -0600

    midway light theme

---
+++ social-media-dashboard-with-theme-switcher-master/script.js
@@ -0,0 +1,24 @@
+const toggle = document.querySelector('.toggle-btn');
+
+const background = document.querySelector('.background');
+const toggleBall = document.querySelector('.toggle-ball');
+const toggleBtn = document.querySelector('.toggle-btn');
+const navTitle = document.querySelector('.nav-title');
+const navSub = document.querySelector('.nav-sub');
+const subTitle = document.querySelector('.sub-title');
+
+
+
+
+
+toggle.addEventListener('click',function () {
+
+    background.classList.toggle('color');
+    toggleBall.classList.toggle('color');
+    toggleBtn.classList.toggle('color');
+    navTitle.classList.toggle('color');
+    navSub.classList.toggle('color');
+    subTitle.classList.toggle('color');
+    
+
+});
