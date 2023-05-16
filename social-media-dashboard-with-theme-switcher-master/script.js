7fb5fd75bf6b379d0269ad7492f6e3482c5aafec
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-05-16 06:53:18 -0600

    project complete,
    revisar codigo para eeconomizarlo

--- social-media-dashboard-with-theme-switcher-master/script.js
+++ social-media-dashboard-with-theme-switcher-master/script.js
@@ -3,9 +3,7 @@ const toggle = document.querySelector('.toggle-btn');
 const background = document.querySelector('.background');
 const toggleBall = document.querySelector('.toggle-ball');
 const toggleBtn = document.querySelector('.toggle-btn');
-const navTitle = document.querySelector('.nav-title');
-const navSub = document.querySelector('.nav-sub');
-const subTitle = document.querySelector('.sub-title');
+
 
 
 
@@ -17,9 +15,4 @@ toggle.addEventListener('click',function () {
     background.classList.toggle('color');
     toggleBall.classList.toggle('color');
     toggleBtn.classList.toggle('color');
-    navTitle.classList.toggle('color');
-    navSub.classList.toggle('color');
-    subTitle.classList.toggle('color');
-    
-
 });
